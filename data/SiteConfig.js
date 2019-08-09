552bf4e75ed565c106ce350cfc34133cfc5c8209
Sean Davey <32485509+RH-sdavey@users.noreply.github.com>
2019-08-09 12:57:49 +0200

    splash revert

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -11,8 +11,8 @@ module.exports = {
   siteDescription:
     "Welcome to SWHY, Choose a learning path from the menu, or scroll down for our latest tutorials", // Website description used for RSS feeds/meta description tag.
   siteCover:
-    "https://picsum.photos/id/1079/4496/3000",
-   //  "https://picsum.photos/id/1002/4312/2868", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
+    "https://picsum.photos/id/1002/4312/2868",
+   //  "https://picsum.photos/id/1079/4496/3000"", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
   siteNavigation: true, // If navigation is enabled the Menu button will be visible
   siteRss: "/rss.xml", // Path to the RSS file.
   siteRssAuthor: "Casper User", // The author name used in the RSS file
