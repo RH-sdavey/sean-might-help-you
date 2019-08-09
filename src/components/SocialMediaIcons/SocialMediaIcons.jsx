3ca06fd48f82ac56299af177bfe6d5c71e2203e5
Sean Davey <32485509+RH-sdavey@users.noreply.github.com>
2019-08-09 11:59:24 +0200

    icons revert

--- src/components/SocialMediaIcons/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons/SocialMediaIcons.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SocialIcon } from "react-social-badge";
+import { SocialIcon } from "react-social-icons";
 import "./SocialMediaIcons.css";
 
 class SocialMediaIcons extends React.Component {
