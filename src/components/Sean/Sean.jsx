bf84381f3b0fc1d34efd560119b821551ccfe2b6
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2019-08-09 09:56:05 +0200

    Create Sean.jsx

---
+++ src/components/Sean/Sean.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Sean.css";
+
+class Sean extends Component {
+  render() {
+    return (
+      <div className="sean">
+        <h1>
+          Sean Sean Sean
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Sean;
